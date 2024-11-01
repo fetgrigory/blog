f5b35893b662befcdee5124117c945fcc4a0a519
fetgrigory <157891679+fetgrigory@users.noreply.github.com>
2024-11-01 15:32:24 +0300

    Commit

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-# Создание экземпляра приложения Flask
+# Creating an instance of the Flask application
 app = Flask(__name__)
 # Настройка базы данных SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///articles.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-# Определение модели данных для статьи
+
+
+# Defining the data model for the article
 class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
@@ -20,28 +22,31 @@ class Article(db.Model):
         return f'<Article {self.id}>'
 
 
-# Определение маршрута для главной страницы
+# Defining the route for the main page
 @app.route('/')
 @app.route('/home')
 def index():
     return render_template("index.html")
 
 
-# Определение маршрута для страницы "About"
+# Defining the route for the "About" page
 @app.route('/about')
 def about():
     return render_template("about.html")
 
 
-# Определение маршрута для страницы "Posts"
+# Determining the route for the "Posts" page
 @app.route('/posts')
 def posts():
     articles = Article.query.order_by(Article.date.desc()).all()
     return render_template("posts.html", articles=articles)
+
+
 @app.route('/posts/<int:id>')
 def post_details(id):
     article = Article.query.get(id)
     return render_template('post_details.html', article=article)
+
 
 @app.route('/posts/<int:id>/delete', methods=['GET', 'POST'])
 def delete_post(id):
@@ -53,12 +58,13 @@ def delete_post(id):
     except Exception as e:
         return "Произошла ошибка при удалении статьи: " + str(e)
 
-# Изминение записи
+
+# Changing an entry
 @app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
 def post_update(id):
     article = Article.query.get(id)
     if request.method == "POST":
-        # Получение данных из формы
+        # Getting data from the form
         article.title = request.form['title']
         article.intro = request.form['intro']
         article.text = request.form['text']
@@ -71,16 +77,16 @@ def post_update(id):
         return render_template("post_update.html", article=article)
 
 
-# Определение маршрута для создания статьи
+# Defining a route to create an article
 @app.route('/write_article', methods=['POST', 'GET'])
 def write_article():
     if request.method == "POST":
-        # Получение данных из формы
+        # Getting data from the form
         title = request.form['title']
         intro = request.form['intro']
         text = request.form['text']
 
-        # Создание новой статьи
+    # Creating a new article
         article = Article(title=title, intro=intro, text=text)
         try:
             db.session.add(article)
@@ -93,9 +99,9 @@ def write_article():
 
 
 if __name__ == "__main__":
-    # Создание всех таблиц в базе данных
+    # Creating all tables in the database
     with app.app_context():
         db.create_all()
 
-    # Запуск приложения в режиме отладки
+# Running the application in debug mode
     app.run(debug=True)
